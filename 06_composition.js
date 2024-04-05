005956ab40a6a7b061d98a4dd0131d8141a2379f
Fred <fredmarsanne@gmail.com>
2024-04-05 12:27:29 +0200

    exo 06 pas fini

--- 06_composition.js
+++ 06_composition.js
@@ -8,7 +8,13 @@
  *  - for, foreach, while, do while sont interdits
  */
 
-const concat = (arr1, arr2) => {}
+//const concat = (arr1, arr2) => {}
+const concat = (arr1, arr2) => [...arr1, ...arr2];
+
+let testarray1 = [1, 2, 3];
+let testarray2 = [4, 5, 6];
+
+console.log(concat(testarray1, testarray2));
 
 /**
  * utiliser l'opérateur de composition ... afin d'ajouter un élément à un tableau
@@ -19,7 +25,10 @@ const concat = (arr1, arr2) => {}
  * - ne pas utiliser la méthode push
  */
 
-const push = (arr, item) => {}
+const push = (arr, item) => [...arr, item];
+
+let testarray3 = [1, 2, 3];
+console.log(push(testarray3, 4));
 
 /**
  * utiliser l'opérateur de composition ... afin de fusionner 2 objets passés en paramètres
@@ -27,7 +36,11 @@ const push = (arr, item) => {}
  * ex: {a: 1, b: 2}, {c: 3, d: 4} => {a: 1, b: 2, c: 3, d: 4}
  */
 
-const merge = (obj1, obj2) => {}
+const merge = (obj1, obj2) => { obj1 };
+
+let testobj1 = {a: 1, b: 2};
+let testobj2 = {c: 3, d: 4};
+console.log(merge(testobj1, testobj2));
 
 /**
  * utiliser l'opérateur de composition ... afin de modifier la propriété name de l'objet
